d35530ba72e37b542ff700bed0e478ea232787b0
davidszcz <szczesniakd@ufl.edu>
2024-05-01 16:06:19 -0400

    updated server

--- app.js
+++ app.js
@@ -282,11 +282,22 @@ app.get('/getcomments', function(req, res) {
 
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+var path = require('path');
 
 http.createServer(function (req, res) {
-  fs.readFile('public/index.html', function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "./public" + q.pathname; // Adjust the file path to include the "public" directory
+  if (filename == "./public/") {
+    filename = "./public/index.html"; // Serve index.html when the root path is accessed
+  }
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
-  })
-}).listen(8888); //the server object listens on port 8888
+  });
+}).listen(8888);
