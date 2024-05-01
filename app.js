b8b0667ce5f004f6cbf2dcff5011f4d01e0cfcf5
davidszcz <szczesniakd@ufl.edu>
2024-05-01 14:59:24 -0400

    createserver reads in index.html file

--- app.js
+++ app.js
@@ -281,9 +281,12 @@ app.get('/getcomments', function(req, res) {
 // });
 
 var http = require('http');
-
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.write('** Welcome to Jukeboxd! **'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+  fs.readFile('public/index.html', function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  })
+}).listen(8888); //the server object listens on port 8888
